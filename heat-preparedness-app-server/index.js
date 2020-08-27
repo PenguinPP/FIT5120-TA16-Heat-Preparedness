4eda55f2238e7d1044b59604dde266a78f894468
hche0097 <49864392+hche0097@users.noreply.github.com>
2020-08-27 21:21:22 +1000

    add-on all 1.0 v

--- heat-preparedness-app-server/index.js
+++ heat-preparedness-app-server/index.js
@@ -26,7 +26,8 @@ app.get('/', (req, res) => res.send("Heat Preparedness Application Server"))
 con.connect(function (err) {
     if (err) throw err;
 })
-//Check for required parameter
+
+//Check for heat threshold for user's suburb required parameter
 app.get('/api/DistrictThreshold/:suburb', [param('suburb').not().isEmpty()],
     async function (req, res) {
         suburbThreshold = await getSpecificThreshold(req.params.suburb), //call appropriate function
@@ -55,14 +56,15 @@ async function getSpecificThreshold(userSuburb) {
     )
 }
 
-app.get('/api/DistrictThreshold/:suburb', [param('suburb').isEmpty()],
+//Check for Weather Forecast for user's Suburb required parameter
+app.get('/api/DistrictThreshold/:suburb', [param('suburb').not().isEmpty()],
     async function (req, res) {
-        defaultsuburbThreshold = await getdefaultThreshold(req.params.suburb), //call appropriate function
-            res.json(defaultsuburbThreshold) //send response
+        weathersuburb = await getweatherforecastsuburb(req.params.suburb), //call appropriate function
+            res.json(weathersuburb) //send response
     })
 
 
-async function getdefaultThreshold(defaultSuburb) {
+async function getweatherforecastsuburb(weatherforecast) {
     con.query(
         `SELECT f.date,
             f.state,
@@ -70,17 +72,141 @@ async function getdefaultThreshold(defaultSuburb) {
             f.min,
             f.max,
             f.avg
-        FROM Forecast f,
+         FROM Forecast f,
             LGA l,
             Suburb s
-        WHERE l.council = s.council
+         WHERE l.council = s.council
             AND l.district = s.district
             AND l.state = s.state
             AND f.council = l.council
             AND f.district = l.district
             AND f.state = l.state
             AND s.suburb = ?;`, //? Represents a parameter
-        defaultSuburb, //parameter you want inserted where the ? is 
+        weatherforecast, //parameter you want inserted where the ? is 
+        function (err, rows, fields) {
+            if (err) throw err
+
+            // console.log('rows:', rows)
+            return rows //return results
+        }
+    )
+}
+
+//Check for weather forecast for Melbourne required parameter
+app.get('/api/DistrictThreshold/:suburb', [param('suburb').isEmpty()],
+    async function (req, res) {
+        defaultweatherforceast = await getdefaultweatherforceast(req.params.suburb), //call appropriate function
+            res.json(defaultweatherforceast) //send response
+    })
+
+
+async function getdefaultweatherforceast() {
+    con.query(
+        `SELECT f.date,
+            f.state,
+            f.area,
+            f.min,
+            f.max,
+            f.avg
+         FROM Forecast f,
+            LGA l,
+            Suburb s
+         WHERE l.council = s.council
+            AND l.district = s.district
+            AND l.state = s.state
+            AND f.council = l.council
+            AND f.district = l.district
+            AND f.state = l.state
+            AND s.surburb = 'Melbourne';`, //? Represents a parameter (parameter you want inserted where the ? is )
+        function (err, rows, fields) {
+            if (err) throw err
+
+            // console.log('rows:', rows)
+            return rows //return results
+        }
+    )
+}
+
+//Check for threshold for Melbourne (default) required parameter
+app.get('/api/DistrictThreshold/:suburb', [param('suburb').isEmpty()],
+    async function (req, res) {
+        defaultsuburbThreshold = await getdefaultThreshold(req.params.suburb), //call appropriate function
+            res.json(defaultsuburbThreshold) //send response
+    })
+
+//con.query(<sql query>, <parameters you want to pass>, function to return)
+
+async function getdefaultThreshold() {
+    con.query(
+        `SELECT threshold
+         FROM District
+         WHERE district_name = 'Melbourne';`, //? Represents a parameter
+        function (err, rows, fields) {
+            if (err) throw err
+
+            // console.log('rows:', rows)
+            return rows //return results
+        }
+    )
+}
+
+//Check for heatwave preparation advice required parameter
+app.get('/api/Advice_pre/:content', [param('content').not().isEmpty()],
+    async function (req, res) {
+        heatprep_advice = await getheatprep(req.params.content), //call appropriate function
+            res.json(heatprep_advice) //send response
+    })
+
+//con.query(<sql query>, <parameters you want to pass>, function to return)
+
+async function getheatprep() {
+    con.query(
+        `SELECT *
+         FROM advice_preparation;`, //? Represents a parameter
+        function (err, rows, fields) {
+            if (err) throw err
+
+            // console.log('rows:', rows)
+            return rows //return results
+        }
+    )
+}
+
+//Check for heatwave advice required parameter
+app.get('/api/Advice/:content', [param('content').not().isEmpty()],
+    async function (req, res) {
+        heatadvice = await getheatadvice(req.params.content), //call appropriate function
+            res.json(heatadvice) //send response
+    })
+
+//con.query(<sql query>, <parameters you want to pass>, function to return)
+
+async function getheatadvice() {
+    con.query(
+        `SELECT *
+         FROM Advice;`, //? Represents a parameter
+        function (err, rows, fields) {
+            if (err) throw err
+
+            // console.log('rows:', rows)
+            return rows //return results
+        }
+    )
+}
+
+//Check for all suburbs required parameter
+app.get('/api/DistrictThreshold/:suburb', [param('suburb').not().isEmpty()],
+    async function (req, res) {
+        allsuburb = await getallsubrb(req.params.suburb), //call appropriate function
+            res.json(allsuburb) //send response
+    })
+
+//con.query(<sql query>, <parameters you want to pass>, function to return)
+
+async function getallsuburb() {
+    con.query(
+        `SELECT suburb
+         FROM Suburb;`,//? Represents a parameter
         function (err, rows, fields) {
             if (err) throw err
 
