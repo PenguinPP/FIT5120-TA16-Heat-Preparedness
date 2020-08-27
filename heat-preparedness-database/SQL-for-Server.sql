bac89bf617e2ca3af5dbe09302adaf3e7b9067cd
Paldeep <penguinpp93@gmail.com>
2020-08-27 14:17:41 +1000

    typo fixes in queries

--- heat-preparedness-database/SQL-for-Server.sql
+++ heat-preparedness-database/SQL-for-Server.sql
@@ -4,7 +4,7 @@
 SELECT *
 FROM LGA;
 --Insert new forecast data (from API)
-INSERT INTO Forecast (date, state, area, min_temp, max_temp, avg_temp)
+INSERT INTO Forecast (date, state, council_name, min_temp, max_temp, avg_temp)
 VALUES (?, ?, ?, ?, ?, ?) ON DUPLICATE KEY
 UPDATE min_temp =
 VALUES(min_temp),
