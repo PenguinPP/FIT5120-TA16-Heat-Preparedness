ca5959c0de4ca6a107ac1f0c18f4086117bc62c8
Paldeep <penguinpp93@gmail.com>
2020-08-28 14:10:55 +1000

    delete app.css

--- heat-preparedness-ui/src/App.js
+++ heat-preparedness-ui/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
-import "./App.css";
 import { Card, CardContent } from '@material-ui/core';
 import Typography from "@material-ui/core/Typography"
 import AppBar from '@material-ui/core/AppBar';
