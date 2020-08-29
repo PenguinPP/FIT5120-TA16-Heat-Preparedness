60b67c1757836ec7e20b3bf3a0d43b2f6cb2d11c
Paldeep <penguinpp93@gmail.com>
2020-08-29 20:24:22 +1000

    create theme and installed font

--- heat-preparedness-ui/src/index.js
+++ heat-preparedness-ui/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const theme = createMuiTheme({
+
+})
+
+ReactDOM.render(<ThemeProvider theme={theme}><App /></ThemeProvider>, document.getElementById('root'));
