c9ff0f029f4546e6e2817cdb80074d304fb048f1
hche0097 <49864392+hche0097@users.noreply.github.com>
2020-08-30 02:29:05 +1000

    update error expection
    1. add up with try-catch function
    2. fix some spelling
    3. still need to discuss Error: ER_BAD_FIELD_ERROR: Unknown column 'f.area' in 'field list'. Seems there is no 'area' variable in our db

--- heat-preparedness-app-server/index.js
+++ heat-preparedness-app-server/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(bodyParser.json())
 const mysql = require('mysql');
-const port = 8080;
+const port = 8080
 
 app.listen(port, () => console.log(`Heat Prep listening on port ${port}!`))
 
@@ -37,17 +37,28 @@ app.get('/api/DistrictThreshold/:suburb', [param('suburb').not().isEmpty()],
 //con.query(<sql query>, <parameters you want to pass>, function to return)
 
 async function getSpecificThreshold(userSuburb) {
-    con.query(
-        `SELECT suburb
+    return new Promise(resultData => {
+        con.query(
+            `SELECT suburb
             FROM Suburb;`, //? Represents a parameter
-        userSuburb, //parameter you want inserted where the ? is 
-        function (err, rows, fields) {
-            if (err) throw err
+            userSuburb, //parameter you want inserted where the ? is 
+            function (error, rows, fields) {
+                if (error)  {
+                //Log error message
+                    console.log(error)
+                    console.log("Failed to retrieve user suburb threshold Data")
+                }
+                try {
+                //set resultData to query result
+                    resultData(result);
 
-            // console.log('rows:', rows)
-            return rows //return results
-        }
-    )
+                } catch (error) {
+                    resultData({}); //Set resultData to empty
+                    console.log("There was an error with the promise");
+                }
+            }          
+        )
+    })
 }
 
 //Check for Weather Forecast for user's Suburb required parameter
@@ -59,35 +70,46 @@ app.get('/api/SuburbForecast/:suburb', [param('suburb').not().isEmpty()],
 
 
 async function getWeatherforecastsuburb(weatherforecast) {
-    con.query(
-        `SELECT f.date,
-            f.state,
-            f.area,
-            f.min,
-            f.max,
-            f.avg
-         FROM Forecast f,
-            LGA l,
-            Suburb s
-         WHERE l.council = s.council
-            AND l.district = s.district
-            AND l.state = s.state
-            AND f.council = l.council
-            AND f.district = l.district
-            AND f.state = l.state
-            AND s.suburb = ?;`, //? Represents a parameter
-        weatherforecast, //parameter you want inserted where the ? is 
-        function (err, rows, fields) {
-            if (err) throw err
+    return new Promise(resultData => {
+        con.query(
+            `SELECT f.date,
+                f.state,
+                f.area,
+                f.min,
+                f.max,
+                f.avg
+            FROM Forecast f,
+                LGA l,
+                Suburb s
+            WHERE l.council = s.council
+                AND l.district = s.district
+                AND l.state = s.state
+                AND f.council = l.council
+                AND f.district = l.district
+                AND f.state = l.state
+                AND s.suburb = ?;`, //? Represents a parameter
+            weatherforecast, //parameter you want inserted where the ? is 
+            function (error, rows, fields) {
+                if (error)  {
+                //Log error message
+                    console.log(error)
+                    console.log("Failed to retrieve user weather forecast Data")
+                }
+                try {
+                //set resultData to query result
+                    resultData(result);
 
-            // console.log('rows:', rows)
-            return rows //return results
-        }
-    )
+                } catch (error) {
+                    resultData({}); //Set resultData to empty
+                    console.log("There was an error with the promise");
+                }
+            }
+        )
+    })
 }
 
 //Check for weather forecast for Melbourne required parameter
-app.get('/api/MelbourneForecast/:suburb',
+app.get('/api/MelbourneForecast',
     async function (req, res) {
         defaultweatherforceast = await getDefaultweatherforceast(), //call appropriate function
             res.json(defaultweatherforceast) //send response
@@ -95,34 +117,45 @@ app.get('/api/MelbourneForecast/:suburb',
 
 
 async function getDefaultweatherforceast() {
-    con.query(
-        `SELECT f.date,
-            f.state,
-            f.area,
-            f.min,
-            f.max,
-            f.avg
-         FROM Forecast f,
-            LGA l,
-            Suburb s
-         WHERE l.council = s.council
-            AND l.district = s.district
-            AND l.state = s.state
-            AND f.council = l.council
-            AND f.district = l.district
-            AND f.state = l.state
-            AND s.surburb = 'Melbourne';`, //? Represents a parameter 
-        function (err, rows, fields) {
-            if (err) throw err
+    return new Promise(resultData => {
+        con.query(
+            `SELECT f.date,
+                f.state,
+                f.area,
+                f.min,
+                f.max,
+                f.avg
+            FROM Forecast f,
+                LGA l,
+                Suburb s
+            WHERE l.council = s.council
+                AND l.district = s.district
+                AND l.state = s.state
+                AND f.council = l.council
+                AND f.district = l.district
+                AND f.state = l.state
+                AND s.surburb = 'Melbourne';`, //? Represents a parameter 
+            function (error, rows, fields) {
+                if (error)  {
+                //Log error message
+                    console.log(error)
+                    console.log("Failed to retrieve melbourne weather forecast Data")
+                }
+                try {
+                //set resultData to query result
+                    resultData(result);
 
-            // console.log('rows:', rows)
-            return rows //return results
-        }
-    )
+                } catch (error) {
+                    resultData({}); //Set resultData to empty
+                    console.log("There was an error with the promise");
+                }
+            }
+        )    
+    })
 }
 
 //Check for threshold for Melbourne (default) required parameter
-app.get('/api/MelbourneThreshold/:suburb',
+app.get('/api/MelbourneThreshold',
     async function (req, res) {
         defaultsuburbThreshold = await getDefaultThreshold(), //call appropriate function
             res.json(defaultsuburbThreshold) //send response
@@ -131,17 +164,28 @@ app.get('/api/MelbourneThreshold/:suburb',
 //con.query(<sql query>, <parameters you want to pass>, function to return)
 
 async function getDefaultThreshold() {
-    con.query(
-        `SELECT threshold
-         FROM District
-         WHERE district_name = 'Melbourne';`, //? Represents a parameter
-        function (err, rows, fields) {
-            if (err) throw err
+    return new Promise(resultData => {
+            con.query(
+            `SELECT threshold
+             FROM ${District}
+             WHERE district_name = 'Melbourne';`, //? Represents a parameter
+            function (error, rows, fields) {
+                if (error) {
+                //Log error message
+                    console.log(error)
+                    console.log("Failed to retrieve melbourne threshold Data")
+                }
+                try {
+                //set resultData to query result
+                    resultData(result);
 
-            // console.log('rows:', rows)
-            return rows //return results
-        }
-    )
+                } catch (error) {
+                    resultData({}); //Set resultData to empty
+                    console.log("There was an error with the promise");
+                }
+            }
+        )
+    })
 }
 
 //Check for heatwave preparation advice required parameter
@@ -154,16 +198,27 @@ app.get('/api/Advice_pre/:content', [param('content').not().isEmpty()],
 //con.query(<sql query>, <parameters you want to pass>, function to return)
 
 async function getHeatprep() {
-    con.query(
-        `SELECT *
-         FROM advice_preparation;`, //? Represents a parameter
-        function (err, rows, fields) {
-            if (err) throw err
+    return new Promise(resultData => {
+        con.query(
+            `SELECT *
+             FROM advice_preparation;`, //? Represents a parameter
+            function (error, rows, fields) {
+                if (error)  {
+                //Log error message
+                    console.log(error)
+                    console.log("Failed to retrieve preparation advice content Data")
+                }
+                try {
+                //set resultData to query result
+                    resultData(result);
 
-            // console.log('rows:', rows)
-            return rows //return results
-        }
-    )
+                } catch (error) {
+                    resultData({}); //Set resultData to empty
+                    console.log("There was an error with the promise");
+                }
+            }
+        )
+    })
 }
 
 //Check for heatwave advice required parameter
@@ -176,16 +231,27 @@ app.get('/api/Advice/:content', [param('content').not().isEmpty()],
 //con.query(<sql query>, <parameters you want to pass>, function to return)
 
 async function getHeatadvice() {
-    con.query(
-        `SELECT *
-         FROM Advice;`, //? Represents a parameter
-        function (err, rows, fields) {
-            if (err) throw err
+    return new Promise(resultData => {
+        con.query(
+            `SELECT *
+             FROM ${Advice};`, //? Represents a parameter
+            function (error, rows, fields) {
+                if (error)  {
+                //Log error message
+                    console.log(error)
+                    console.log("Failed to retrieve advice content Data")
+                }
+                try {
+                //set resultData to query result
+                    resultData(result);
 
-            // console.log('rows:', rows)
-            return rows //return results
-        }
-    )
+                } catch (error) {
+                    resultData({}); //Set resultData to empty
+                    console.log("There was an error with the promise");
+                }
+            }
+        )
+    })
 }
 
 //Check for all suburbs required parameter
@@ -201,7 +267,7 @@ async function getAllsuburb() {
     return new Promise(resultData => {
         con.query(
             `SELECT suburb
-         FROM Suburb;`,//? Represents a parameter
+             FROM ${Suburb};`,//? Represents a parameter
             function (error, result, fields) {
                 if (error) {
                     //Log error message
