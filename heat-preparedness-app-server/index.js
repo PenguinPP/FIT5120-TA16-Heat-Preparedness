7e244db5dcf21f1c26d2e8fce7ee9f3003a49c7d
Paldeep <penguinpp93@gmail.com>
2020-08-30 12:15:03 +1000

    fixed variable error and restructured connections

--- heat-preparedness-app-server/index.js
+++ heat-preparedness-app-server/index.js
@@ -10,53 +10,54 @@ const port = 8080;
 
 app.listen(port, () => console.log(`Heat Prep listening on port ${port}!`))
 
-const con = mysql.createConnection({
-    host: process.env.DB_ENDPOINT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-
 //Example Weather API Call
 //https://api.openweathermap.org/data/2.5/onecall?lat=-38.2551&lon=144.6726&exclude=hourly,current,minutely&units=metric&appid=process.env.OPEN_WEATHER_ONE_CALL_API
 
 app.get('/', (req, res) => res.send("Heat Preparedness Application Server"))
 
-con.connect(function (err) {
-    if (err) throw err;
-})
+
 
 //Check for heat threshold for user's suburb required parameter
 app.get('/api/DistrictThreshold/:suburb', [param('suburb').not().isEmpty()],
     async function (req, res) {
-        suburbThreshold = await getSpecificThreshold(req.params.suburb), //call appropriate function
-            res.json(suburbThreshold) //send response
+
+        const dbConnection = mysql.createConnection({ //open db connection
+            host: process.env.DB_ENDPOINT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+
+        suburbThreshold = await getSpecificThreshold(dbConnection, req.params.suburb) //call appropriate function
+        dbConnection.end() //close db connection
+        res.json(suburbThreshold) //send response
     })
 
 //con.query(<sql query>, <parameters you want to pass>, function to return)
 
-async function getSpecificThreshold(userSuburb) {
+async function getSpecificThreshold(dbConnection, userSuburb) {
     return new Promise(resultData => {
-        con.query(
-            `SELECT suburb
-            FROM Suburb;`, //? Represents a parameter
+        dbConnection.query(
+            `SELECT d.threshold
+             FROM District d, Suburb s
+             WHERE s.district = d.district_name
+             AND s.suburb = ?;`, //? Represents a parameter
             userSuburb, //parameter you want inserted where the ? is 
-            function (error, rows, fields) {
-                if (error)  {
-                //Log error message
+            function (error, result, fields) {
+                if (error) {
+                    //Log error message
                     console.log(error)
                     console.log("Failed to retrieve user suburb threshold Data")
                 }
                 try {
-                //set resultData to query result
+                    //set resultData to query result
                     resultData(result);
 
                 } catch (error) {
                     resultData({}); //Set resultData to empty
                     console.log("There was an error with the promise");
                 }
-            }          
+            }
         )
     })
 }
@@ -64,17 +65,24 @@ async function getSpecificThreshold(userSuburb) {
 //Check for Weather Forecast for user's Suburb required parameter
 app.get('/api/SuburbForecast/:suburb', [param('suburb').not().isEmpty()],
     async function (req, res) {
-        weathersuburb = await getWeatherforecastsuburb(req.params.suburb), //call appropriate function
-            res.json(weathersuburb) //send response
+        const dbConnection = mysql.createConnection({ //open db connection
+            host: process.env.DB_ENDPOINT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+        weatherSuburb = await getWeatherForecastSuburb(dbConnection, req.params.suburb) //call appropriate function
+        dbConnection.end() //close db connection   
+        res.json(weatherSuburb) //send response
     })
 
 
-async function getWeatherforecastsuburb(weatherforecast) {
+async function getWeatherForecastSuburb(dbConnection, weatherforecast) {
     return new Promise(resultData => {
-        con.query(
+        dbConnection.query(
             `SELECT f.date,
                 f.state,
-                f.area,
+                f.council,
                 f.min,
                 f.max,
                 f.avg
@@ -89,14 +97,14 @@ async function getWeatherforecastsuburb(weatherforecast) {
                 AND f.state = l.state
                 AND s.suburb = ?;`, //? Represents a parameter
             weatherforecast, //parameter you want inserted where the ? is 
-            function (error, rows, fields) {
-                if (error)  {
-                //Log error message
+            function (error, result, fields) {
+                if (error) {
+                    //Log error message
                     console.log(error)
                     console.log("Failed to retrieve user weather forecast Data")
                 }
                 try {
-                //set resultData to query result
+                    //set resultData to query result
                     resultData(result);
 
                 } catch (error) {
@@ -111,17 +119,28 @@ async function getWeatherforecastsuburb(weatherforecast) {
 //Check for weather forecast for Melbourne required parameter
 app.get('/api/MelbourneForecast',
     async function (req, res) {
-        defaultweatherforceast = await getDefaultweatherforceast(), //call appropriate function
-            res.json(defaultweatherforceast) //send response
+
+        const dbConnection = mysql.createConnection({ //open db connection
+            host: process.env.DB_ENDPOINT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+
+        defaultWeatherForecast = await getDefaultWeatherForecast(dbConnection) //call appropriate function
+
+        dbConnection.end() //close db connection   
+
+        res.json(defaultWeatherForecast) //send response
     })
 
 
-async function getDefaultweatherforceast() {
+async function getDefaultWeatherForecast(dbConnection) {
     return new Promise(resultData => {
-        con.query(
+        dbConnection.query(
             `SELECT f.date,
                 f.state,
-                f.area,
+                f.council,
                 f.min,
                 f.max,
                 f.avg
@@ -134,15 +153,15 @@ async function getDefaultweatherforceast() {
                 AND f.council = l.council
                 AND f.district = l.district
                 AND f.state = l.state
-                AND s.surburb = 'Melbourne';`, //? Represents a parameter 
-            function (error, rows, fields) {
-                if (error)  {
-                //Log error message
+                AND s.suburb = 'Melbourne';`, //? Represents a parameter 
+            function (error, result, fields) {
+                if (error) {
+                    //Log error message
                     console.log(error)
                     console.log("Failed to retrieve melbourne weather forecast Data")
                 }
                 try {
-                //set resultData to query result
+                    //set resultData to query result
                     resultData(result);
 
                 } catch (error) {
@@ -150,33 +169,43 @@ async function getDefaultweatherforceast() {
                     console.log("There was an error with the promise");
                 }
             }
-        )    
+        )
     })
 }
 
 //Check for threshold for Melbourne (default) required parameter
 app.get('/api/MelbourneThreshold',
     async function (req, res) {
-        defaultsuburbThreshold = await getDefaultThreshold(), //call appropriate function
-            res.json(defaultsuburbThreshold) //send response
+        const dbConnection = mysql.createConnection({ //open db connection
+            host: process.env.DB_ENDPOINT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+
+        defaultSuburbThreshold = await getDefaultThreshold(dbConnection) //call appropriate function
+
+        dbConnection.end() //close db connection   
+
+        res.json(defaultSuburbThreshold) //send response
     })
 
 //con.query(<sql query>, <parameters you want to pass>, function to return)
 
-async function getDefaultThreshold() {
+async function getDefaultThreshold(dbConnection) {
     return new Promise(resultData => {
-            con.query(
+        dbConnection.query(
             `SELECT threshold
              FROM District
-             WHERE district_name = 'Melbourne';`, //? Represents a parameter
-            function (error, rows, fields) {
+             WHERE district_name = 'Central District';`, //? Represents a parameter
+            function (error, result, fields) {
                 if (error) {
-                //Log error message
+                    //Log error message
                     console.log(error)
                     console.log("Failed to retrieve melbourne threshold Data")
                 }
                 try {
-                //set resultData to query result
+                    //set resultData to query result
                     resultData(result);
 
                 } catch (error) {
@@ -189,27 +218,38 @@ async function getDefaultThreshold() {
 }
 
 //Check for heatwave preparation advice required parameter
-app.get('/api/Advice_pre/:content', [param('content').not().isEmpty()],
+app.get('/api/Advice_pre',
     async function (req, res) {
-        heatprep_advice = await getHeatprep(req.params.content), //call appropriate function
-            res.json(heatprep_advice) //send response
+
+        const dbConnection = mysql.createConnection({ //open db connection
+            host: process.env.DB_ENDPOINT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+
+        heatPrepAdvice = await getHeatPrep(dbConnection) //call appropriate function
+
+        dbConnection.end() //close db connection   
+
+        res.json(heatPrepAdvice) //send response
     })
 
 //con.query(<sql query>, <parameters you want to pass>, function to return)
 
-async function getHeatprep() {
+async function getHeatPrep(dbConnection) {
     return new Promise(resultData => {
-        con.query(
+        dbConnection.query(
             `SELECT *
-             FROM advice_preparation;`, //? Represents a parameter
-            function (error, rows, fields) {
-                if (error)  {
-                //Log error message
+             FROM Advice_pre;`, //? Represents a parameter
+            function (error, result, fields) {
+                if (error) {
+                    //Log error message
                     console.log(error)
                     console.log("Failed to retrieve preparation advice content Data")
                 }
                 try {
-                //set resultData to query result
+                    //set resultData to query result
                     resultData(result);
 
                 } catch (error) {
@@ -222,31 +262,42 @@ async function getHeatprep() {
 }
 
 //Check for heatwave advice required parameter
-app.get('/api/Advice/:content', [param('content').not().isEmpty()],
+app.get('/api/Advice',
     async function (req, res) {
-        heatadvice = await getHeatadvice(req.params.content), //call appropriate function
-            res.json(heatadvice) //send response
+
+        const dbConnection = mysql.createConnection({ //open db connection
+            host: process.env.DB_ENDPOINT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+
+        heatAdvice = await getHeatAdvice(dbConnection) //call appropriate function
+
+        dbConnection.end() //close db connection  
+
+        res.json(heatAdvice) //send response
     })
 
 //con.query(<sql query>, <parameters you want to pass>, function to return)
 
-async function getHeatadvice() {
-    return new Promise(resultData => {
-        con.query(
+async function getHeatAdvice(dbConnection) {
+    return new Promise(adviceData => {
+        dbConnection.query(
             `SELECT *
              FROM Advice;`, //? Represents a parameter
-            function (error, rows, fields) {
-                if (error)  {
-                //Log error message
+            function (error, result, fields) {
+                if (error) {
+                    //Log error message
                     console.log(error)
                     console.log("Failed to retrieve advice content Data")
                 }
                 try {
-                //set resultData to query result
-                    resultData(result);
+                    //set resultData to query result
+                    adviceData(result);
 
                 } catch (error) {
-                    resultData({}); //Set resultData to empty
+                    adviceData({}); //Set resultData to empty
                     console.log("There was an error with the promise");
                 }
             }
@@ -257,15 +308,25 @@ async function getHeatadvice() {
 //Check for all suburbs required parameter
 app.get('/api/SuburbList',
     async function (req, res) {
-        allsuburb = await getAllsuburb(req.params.suburb), //call appropriate function
-            res.json(allsuburb) //send response
+        const dbConnection = mysql.createConnection({ //open db connection
+            host: process.env.DB_ENDPOINT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+
+        allsuburb = await getAllsuburb(dbConnection) //call appropriate function
+
+        res.json(allsuburb) //send response
+
+        dbConnection.end() //close db connection  
     })
 
 //con.query(<sql query>, <parameters you want to pass>, function to return)
 
-async function getAllsuburb() {
+async function getAllsuburb(dbConnection) {
     return new Promise(resultData => {
-        con.query(
+        dbConnection.query(
             `SELECT suburb
          FROM Suburb;`,//? Represents a parameter
             function (error, result, fields) {
