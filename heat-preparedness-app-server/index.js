0441ae2e3622da49fe3ec08a7db88d4f000bb69c
hche0097 <49864392+hche0097@users.noreply.github.com>
2020-08-30 12:28:11 +1000

    fix rows to result in con.query(function())

--- heat-preparedness-app-server/index.js
+++ heat-preparedness-app-server/index.js
@@ -235,7 +235,7 @@ async function getHeatadvice() {
         con.query(
             `SELECT *
              FROM Advice;`, //? Represents a parameter
-            function (error, rows, fields) {
+            function (error, result, fields) {
                 if (error)  {
                 //Log error message
                     console.log(error)
