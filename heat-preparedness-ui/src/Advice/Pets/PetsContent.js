2a856746f8f7edd3765e760858d0798fcadfddd6
Tan <ztan0048@student.monash.edu>
2020-09-17 17:34:05 +1000

    add icons for pets

--- heat-preparedness-ui/src/Advice/Pets/PetsContent.js
+++ heat-preparedness-ui/src/Advice/Pets/PetsContent.js
@@ -6,13 +6,18 @@ import {
   Typography,
   List,
   ListItem,
-  ListItemIcon,
   CardMedia,
 } from "@material-ui/core";
 import CheckIcon from "@material-ui/icons/Check";
 import ClearIcon from "@material-ui/icons/Clear";
 // import noCoffeeAlcohol from "./images/nocoffeealcohol.png";
 // import noExercise from "./images/no-exercise.png";
+import onTile from "./images/onTile.png";
+import coolWater from "./images/coolWater.png";
+import wetFeet from "./images/wetFeet.png";
+import noPetInCar from "./images/noPetInCar.png"
+import sunCream from "./images/sunCream.png"
+import walkPet from "./images/walkPet.png"
 import Grid from "@material-ui/core/Grid";
 import { useTheme } from "@material-ui/core/styles";
 
@@ -23,11 +28,27 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.tertiary.main,
     color: theme.palette.tertiary.contrastText,
   },
-  mediaCoffee: {
+  mediaTile: {
     height: 200,
     width: 200,
   },
-  mediaExercise: {
+  mediaWater: {
+    height: 200,
+    width: 200,
+  },
+  mediaFeet: {
+    height: 200,
+    width: 200,
+  },
+  mediaNoPetinCar: {
+    height: 200,
+    width: 200,
+  },
+  mediaSuncream: {
+    height: 200,
+    width: 200,
+  },
+  mediaWalkPet: {
     height: 200,
     width: 200,
   },
@@ -55,18 +76,31 @@ export function StayHome() {
   return (
     <Card raised={true}>
       <CardContent className={classes.cardContentStyle}>
-        <Typography variant="h5">{content[0]["title"]}</Typography>
-        <List>
-          {content[0]["content"].map((item, index) => (
-            <React.Fragment>
-              <ListItem id={index}>
-                <Typography variant="body1">
-                  {index + 1}. {item}
-                </Typography>
-              </ListItem>
-            </React.Fragment>
-          ))}
-        </List>
+        <Grid container alignItems="flex-start">
+          <Grid item xs={12}>
+            <Typography variant="h5">{content[0]["title"]}</Typography>
+            <List>
+              {content[0]["content"].map((item, index) => (
+                <React.Fragment>
+                  <ListItem id={index}>
+                    <Typography variant="body1">
+                      {index + 1}. {item}
+                    </Typography>
+                  </ListItem>
+                </React.Fragment>
+              ))}
+            </List>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <CardMedia image={onTile} className={classes.mediaTile} />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <CardMedia image={coolWater} className={classes.mediaWater} />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <CardMedia image={wetFeet} className={classes.mediaFeet} />
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
@@ -79,7 +113,7 @@ export function OutDoor() {
       title: "If your pet is outside or you need to walk them",
       content: [
         "NEVER leave your pet in the car in warm weather.",
-        "Your fair-skinned pet needs a special sunscreen for animals to protect it from the discomfort of sunburn and the added risk of skin cancer.",
+        "Your fair-skinned pet needs a special sunscreen for animals to protect it from the discomfort of sunburn and the added risk of skin cancer.Especially their noses.",
         "You can walk them early morning or evening or a ground that feels hot to touch can do serious damage to your pooch’s paws.",
       ],
     },
@@ -88,18 +122,31 @@ export function OutDoor() {
   return (
     <Card raised={true}>
       <CardContent className={classes.cardContentStyle}>
-        <Typography variant="h5">{content[0]["title"]}</Typography>
-        <List>
-          {content[0]["content"].map((item, index) => (
-            <React.Fragment>
-              <ListItem id={index}>
-                <Typography variant="body1">
-                  {index + 1}. {item}
-                </Typography>
-              </ListItem>
-            </React.Fragment>
-          ))}
-        </List>
+        <Grid container alignItems="flex-start">
+          <Grid item xs={12}>
+            <Typography variant="h5">{content[0]["title"]}</Typography>
+            <List>
+              {content[0]["content"].map((item, index) => (
+                <React.Fragment>
+                  <ListItem id={index}>
+                    <Typography variant="body1">
+                      {index + 1}. {item}
+                    </Typography>
+                  </ListItem>
+                </React.Fragment>
+              ))}
+            </List>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <CardMedia image={noPetInCar} className={classes.mediaNoPetinCar} />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <CardMedia image={sunCream} className={classes.mediaSuncream} />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <CardMedia image={walkPet} className={classes.mediaWalkPet} />
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
