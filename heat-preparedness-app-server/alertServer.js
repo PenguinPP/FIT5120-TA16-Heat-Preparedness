30129c9fee383b683f98e6ca0f5a00f8905a8bc8
Tan <ztan0048@student.monash.edu>
2020-10-19 21:16:37 +1100

    record notification on database and set it send notification everyday 9am and 6pm

--- heat-preparedness-app-server/alertServer.js
+++ heat-preparedness-app-server/alertServer.js
@@ -30,8 +30,29 @@ app.listen(port, () =>
   console.log(`Heat Alert Server listening on port ${port}!`)
 );
 
-//run sendAlerts function
-sendAlerts();
+const schedule = require("node-schedule");
+//check and sent notification everyday at 9am
+const dailyNotification9am = () => {
+  schedule.scheduleJob(
+  "0 0 9 * * *",
+  function(){
+    sendAlerts();
+  });
+}
+
+//check and sent notification everyday at 6pm
+const dailyNotification6pm = () => {
+  schedule.scheduleJob(
+  "0 0 18 * * *",
+  function(){
+    sendAlerts();
+  });
+}
+
+//run the schedule and send notification every day
+dailyNotification9am();
+dailyNotification6pm();
+
 
 async function sendAlerts() {
   //Create db connection
@@ -42,12 +63,20 @@ async function sendAlerts() {
     database: process.env.DB_NAME,
   });
   
+  //get all users' information that who subscribe for notification
+  //and there will be a heat wave after one or three days 
+  //according to their selection
   const userChoice_1day = await getuserChoice(dbConnection,1);
   const userChoice_3day = await getuserChoice(dbConnection,3);
 
+  //create two array to collect all user's information who 
+  //didn't get notification according to their selection
   var user_list_1day = new Array();
   var user_list_3day = new Array();
 
+  //search the correspoding notification record in database
+  //when the result is empty, it means this user should receive notification but haven't got it yet
+  //thus, user's information will be stored in array and send notification later
   for (i=0;i<userChoice_1day.length;i++){
     var checkResult = await checkNotification(dbConnection,userChoice_1day[i],1);
     if (checkResult.length == 0){
@@ -62,72 +91,68 @@ async function sendAlerts() {
     }
   }
 
-  console.log("send 1 day notifications");
-
+  //start to send notification one by one for who subscribe 1 day notification
   for (i=0;i<user_list_1day.length;i++){
     var notiBody = "There will be a heat wave tomorrow in " + 
     user_list_1day[i]["suburb"] + ", please prepare for it and keep safe."
 
+    //construct notification payload
     var payload = JSON.stringify({
       title: "Notification!",
       body: notiBody,
       icon: "https://vic-heat-deployment-files.s3-ap-southeast-2.amazonaws.com/vic-heat-noti.png",
     });
 
-    console.log(payload);
-
+    //construct webpush details
     let webPushDetails = {};
     webPushDetails["endpoint"] = user_list_1day[i]["end_point"];
     webPushDetails["keys"] = {};
     webPushDetails["keys"]["p256dh"] = user_list_1day[i]["p256dh"];
     webPushDetails["keys"]["auth"] = user_list_1day[i]["auth"];
 
-    console.log(webPushDetails);
-
-    webpush
+    //send notification
+    await webpush
     .sendNotification(webPushDetails,payload)
-    .then((result) => console.log(result))
+    .then((result) => {
+      //record the notification if the webpush success
+      if (result["statusCode"] == 201){
+        insertNotification(dbConnection,userChoice_1day[i],1);
+      }
+    })
     .catch((e) => console.log(e.stack));
 
   }
 
-  console.log("send 3 day notifications");
-
+  //start to send notification one by one for who subscribe 3 day notification
   for (i=0;i<user_list_3day.length;i++){
-    var notiBody = "There will be a heat wave tomorrow in " + 
-    user_list_3day[i]["suburb"] + ", please prepare for it and keep safe."
+    var notiBody = "There will be a heat wave in " + user_list_3day[i]["suburb"] + " on " + 
+    user_list_3day[i]["date"] + ", please prepare for it and keep safe."
 
+    //construct notification payload
     var payload = JSON.stringify({
       title: "Notification!",
       body: notiBody,
+      icon: "https://vic-heat-deployment-files.s3-ap-southeast-2.amazonaws.com/vic-heat-noti.png",
     });
 
-    console.log(payload);
-
+    //construct webpush details
     let webPushDetails = {};
     webPushDetails["endpoint"] = user_list_3day[i]["end_point"];
     webPushDetails["keys"] = {};
     webPushDetails["keys"]["p256dh"] = user_list_3day[i]["p256dh"];
     webPushDetails["keys"]["auth"] = user_list_3day[i]["auth"];
 
-    console.log(webPushDetails);
-
-    webpush
+    //send notification
+    await webpush
     .sendNotification(webPushDetails,payload)
-    .then((result) => console.log(result))
+    .then((result) => {
+      if (result["statusCode"] == 201){
+        insertNotification(dbConnection,userChoice_3day[i],3);
+      }
+    })
     .catch((e) => console.log(e.stack));
 
   }
-
-  
-
-
-
-  /*Insert code here.
-    Check for suburbs that have heat waves 3 days from now or 1 day from now
-    Check for users that have not received notifications for these suburbs
-    Send notifications to users
-    Store record of notification in db*/
 
   //close db connection
   dbConnection.end();
@@ -139,7 +164,7 @@ async function getuserChoice(dbConnection,targ_day){
   if (targ_day == 1){
     return new Promise((resultData) =>{
       dbConnection.query(
-        `select end_point,p256dh,auth,suburb,u.subscrip_id,u.suburb_id,c.council_id,noti_1day,noti_3day,threshold,avg,date 
+        `select end_point,p256dh,auth,suburb,u.subscrip_id,u.suburb_id,c.council_id,noti_1day,noti_3day,threshold,avg,date_format(date, '%W %d-%m-%Y') as date
           from District as d join Council as c on d.district_id = c.district_id
           join Forecast as f on c.council_id = f.council_id
           join User_choice as u on u.council_id = c.council_id
@@ -167,7 +192,7 @@ async function getuserChoice(dbConnection,targ_day){
   else{
     return new Promise((resultData) =>{
       dbConnection.query(
-        `select end_point,p256dh,auth,suburb,u.subscrip_id,u.suburb_id,c.council_id,noti_1day,noti_3day,threshold,avg,date 
+        `select end_point,p256dh,auth,suburb,u.subscrip_id,u.suburb_id,c.council_id,noti_1day,noti_3day,threshold,avg,date_format(date, '%W %d-%m-%Y') as date
           from District as d join Council as c on d.district_id = c.district_id
           join Forecast as f on c.council_id = f.council_id
           join User_choice as u on u.council_id = c.council_id
@@ -243,4 +268,33 @@ async function checkNotification(dbConnection,userChoice,targ_day){
   }
 
   
+}
+
+async function insertNotification(dbConnection,userInfo,targ_day){
+  
+  if (targ_day == 1){
+    dbConnection.query(
+      `insert into Notification_1day (subscrip_id,suburb_id,council_id,noti_time,noti_targ_date)
+        values (?,?,?,now(),date_add(curdate(),interval 1 day));`,
+      [userInfo["subscrip_id"],userInfo["suburb_id"],userInfo["council_id"]],
+      function (error, result, fields){
+        if (error){
+          console.log(error);
+          console.log("Failed to record notification");
+        }
+      }
+    )
+  }else{
+    dbConnection.query(
+      `insert into Notification_3day (subscrip_id,suburb_id,council_id,noti_time,noti_targ_date)
+        values (?,?,?,now(),date_add(curdate(),interval 3 day));`,
+      [userInfo["subscrip_id"],userInfo["suburb_id"],userInfo["council_id"]],
+      function (error, result, fields){
+        if (error){
+          console.log(error);
+          console.log("Failed to record notification");
+        }
+      }
+    )
+  }
 }
